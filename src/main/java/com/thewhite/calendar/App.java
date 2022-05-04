7396d309a21e164303a3cf252da63b4a6615da60
Fedor Ishchenko <65521226+WelcomeDev@users.noreply.github.com>
2022-05-04 17:12:03 +1000

    Initial commit

---
+++ src/main/java/com/thewhite/calendar/App.java
@@ -0,0 +1,38 @@
+package com.thewhite.calendar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+/**
+ * Created on 4/15/2022
+ *
+ * @author Fedor Ishchenko
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(App.class);
+        Environment env = SpringApplication.run(App.class, args).getEnvironment();
+        try {
+            logger.info("\n----------------------------------------------------------\n" +
+                                "\tApplication '{}' is running! \n" +
+                                "\tLocal: \t\thttp://localhost:{}\n" +
+                                "\tExternal: \thttp://{}:{}\n" +
+                                "----------------------------------------------------------",
+                        env.getProperty("spring.application.name"),
+                        env.getProperty("server.port"),
+                        InetAddress.getLocalHost().getHostAddress(),
+                        env.getProperty("server.port"),
+                        Arrays.toString(env.getActiveProfiles()));
+        } catch (UnknownHostException e) {
+            logger.error("Exception host-address", e);
+        }
+    }
+}
